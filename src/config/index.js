208024f00bd234c33d1f52b56aeb29367faa86de
Allan <alan92goncalves@gmail.com>
2021-02-04 12:24:00 -0300

    corrigindo url do backand

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://projeto-aluraflix-react.herokuapp.com/categorias';
+  : 'https://projeto-aluraflix-react.herokuapp.com';
 
 export default {
   URL_BACKEND_TOP,
